cf8b6d426bd3f29005ff16ebc6165c4b23dc65fa
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-07-11 10:49:07 +0300

    cors policy update

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // Define CORS options
 const corsOptions = {
-  origin: ["https://smartcsvtool.com", "http://localhost:3000"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: [
+    "https://smartcsvtool.com",
+    "https://www.smartcsvtool.com",
+    "http://localhost:3000",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"], // add any other headers your client sends
 };
 
 // Apply CORS middleware globally
@@ -23,17 +28,15 @@ app.options("*", cors(corsOptions));
 app.get("/", (req, res) => {
   res.send("We are on the home page");
 });
-app.get("/", (req, res) => {
-  res.send("We are on the home page");
+
+app.use("/api/v1/csv", require("./routes/csvStringSimilarityRoute"));
+app.use("/api/v1/contact", require("./routes/emailHandlerRoute"));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
-
-// Import routes
-const csvStringSimilarityRoute = require("./routes/csvStringSimilarityRoute");
-const emailHandlerRoute = require("./routes/emailHandlerRoute");
-
-// Explicitly set CORS headers for specific routes if needed
-app.use("/api/v1/csv", cors(corsOptions), csvStringSimilarityRoute);
-app.use("/api/v1/contact", cors(corsOptions), emailHandlerRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));

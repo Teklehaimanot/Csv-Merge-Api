e9bde641644d42b1e1ca6740633b50d47bff676a
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-07-11 09:44:41 +0300

    some update on nodemailer

--- controllers/emailHandleController.js
+++ controllers/emailHandleController.js
@@ -6,10 +6,15 @@ const emailHandler = async (req, res) => {
 
     const { name, email, message } = req.body.formData;
     const transporter = nodemailer.createTransport({
-      service: "gmail", // use your email service
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER, // your email
         pass: process.env.EMAIL_PASS, // your email password or app-specific password
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
